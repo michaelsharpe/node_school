538263e29c343b1096dcf32caea321abeb37491d
michaelsharpe <m.sharpe@project89.org>
2014-01-24 15:39:58 -0500

    Completed exercise 10 of Stream Adventure.

---
+++ stream_adventure/duplexer_redux.js
@@ -0,0 +1,16 @@
+var duplex = require("duplexer");
+var through = require("through");
+
+module.exports = function(counter) {
+  var counts = {};
+  var input = through(write, end);
+  return duplex(input, counter);
+
+  function write (data){
+    counts[data.country] = (counts[data.country] || 0) + 1
+  }
+
+  function end (){
+    counter.setCounts(counts)
+  }
+}
